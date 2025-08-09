2bf337d7222e46fd602c5f31a0eab72c58377b8f
PavelParvadov <parvadov.pavel@gmail.com>
2025-08-09 17:55:43 +0300

    add : price-producer cfg

--- price-producer/cmd/price-producer/main.go
+++ price-producer/cmd/price-producer/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/PavelParvadov/price_analyzer/price-producer/internal/config"
+	"github.com/PavelParvadov/price_analyzer/price-producer/pkg/logger"
 	"go.uber.org/zap"
-	"price_analyzer/price-producer/pkg/logger"
 )
 
 func main() {
 	log := logger.NewLogger()
-	log.Info("test", zap.Any("any", "pavel"))
+	log.Info("test", zap.Any("msg", "msg"))
+	cfg := config.GetInstance()
+	log.Info("config", zap.Any("cfg", cfg))
 }
---
+++ price-producer/internal/config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	PriceConfig PriceGrpcConfig `yaml:"PriceGrpcConfig"`
+	KafkaConfig KafkaConfig     `yaml:"KafkaConfig"`
+	Producer    ProducerConfig  `yaml:"ProducerConfig"`
+}
+
+type PriceGrpcConfig struct {
+	Port int `yaml:"PricePort" env:"PRICE_PORT"`
+}
+
+type KafkaConfig struct {
+	Addresses string `yaml:"addresses" env:"ADDRESSES"`
+	Topic     string `yaml:"topic" env:"TOPIC"`
+}
+type ProducerConfig struct {
+	Tickers           []string `yaml:"tickers" env:"TICKERS" env-separator:","`
+	IntervalMs        int      `yaml:"intervalMs" env:"INTERVAL_MS"`
+	InitialPrice      float64  `yaml:"initialPrice" env:"INITIAL_PRICE"`
+	VolatilityPercent float64  `yaml:"volatilityPercent" env:"VOLATILITY_PERCENT"`
+}
+
+var instance *Config
+var once sync.Once
+
+func GetInstance() *Config {
+	once.Do(func() {
+		path := FetchConfigPath()
+		instance = LoadConfigByPath(path)
+	})
+	return instance
+}
+
+func LoadConfigByPath(path string) *Config {
+	var cfg Config
+
+	if path != "" {
+		if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+			panic(err)
+		}
+	}
+
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		panic(err)
+	}
+
+	return &cfg
+}
+
+func FetchConfigPath() string {
+	var res string
+	flag.StringVar(&res, "config-path", "", "load config from path")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
